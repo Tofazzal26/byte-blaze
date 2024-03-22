31addd2be5cb17162a3e0118641ecbd1c0f8dda9
Tofazzal26 <afranislamabir6789@gmail.com>
2024-03-22 09:56:32 +0600

    blog data dynamic validation finish

---
+++ src/Components/Blog/Blog.jsx
@@ -0,0 +1,30 @@
+import { NavLink } from "react-router-dom";
+import notFound from "../../assets/404.jpg";
+const Blog = ({ card }) => {
+  const { id, cover_image, title, description, created_at } = card;
+
+  return (
+    <NavLink
+      rel="noopener noreferrer"
+      to={`/blog/${id}`}
+      className="max-w-sm mx-auto group hover:no-underline focus:no-underline bg-gray-900"
+    >
+      <img
+        role="presentation"
+        className="object-cover w-full rounded h-44 bg-gray-500"
+        src={cover_image || notFound}
+      />
+      <div className="p-6 space-y-2">
+        <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">
+          {title}
+        </h3>
+        <span className="text-xs text-gray-400">
+          {new Date(card.created_at).toLocaleDateString()}
+        </span>
+        <p>{description}</p>
+      </div>
+    </NavLink>
+  );
+};
+
+export default Blog;
